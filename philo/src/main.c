1189b8b78ac0b06f808938c98890c69d2ff41dff
mdoll <mdoll@student.42wolfsburg.de>
2023-04-15 11:47:50 +0200

    feat: filling structs

--- philo/src/main.c
+++ philo/src/main.c
@@ -15,35 +15,48 @@
 #include <pthread/pthread.h>
 #include <strings.h>
 #include <sys/time.h>
+#include <stdlib.h>
 
-static void	fill_struct(t_philo *phil, char **argv, int argc)
-{
-	phil->number_of_philo = (int)atolong(argv[1]);
-	phil->time_to_die = (int)atolong(argv[2]);
-	phil->time_to_eat = (int)atolong(argv[3]);
-	phil->time_to_sleep = (int)atolong(argv[4]);
+static void	fill_struct(t_data *data, char **argv, int argc) {
+	int	i;
+
+	data->number_of_philo = (int) atolong(argv[1]);
+	data->time_to_die = (int) atolong(argv[2]);
+	data->time_to_eat = (int) atolong(argv[3]);
+	data->time_to_sleep = (int) atolong(argv[4]);
 	if (argc == 6)
-		phil->how_much_eat = (int)atolong(argv[5]);
+		data->how_much_eat = (int) atolong(argv[5]);
 	else
 		phil->how_much_eat = -1;
 }
 
+static void	init_philosopher(t_philo *philo, t_data *data, t_time *start_time, int id)
+{
+	philo->data = data;
+	philo->start_time = start_time;
+	philo->id = id;
+	philo->nb_eaten = 0;
+}
+
 int	main(int argc, char **argv)
 {
-	t_philo			phil;
+	t_data			data;
 	pthread_t		tid[100000];
 	int				i;
 	t_time			start_time;
+	t_philo			philo;
 
 	if (input_check(argc, argv))
 		return (1);
 	i = 0;
-	fill_struct(&phil, argv, argc);
+	fill_struct(&data, argv, argc);
 	gettimeofday(&start_time, NULL);
 	printf("number of philos: %i\ntime to die: %d\ntime to eat: %d\ntime to sleep %d\nhow much eat: %d\n", phil.number_of_philo, phil.time_to_die, phil.time_to_eat, phil.time_to_sleep, phil.how_much_eat);
 	while (i < phil.number_of_philo)
 	{
-		if (pthread_create(&tid[i], NULL, &philosopher, &start_time) != 0)
+		philo = *(t_philo *)malloc(sizeof(philo));
+		init_philosopher(&philo, &data, &start_time, i);
+		if (pthread_create(&tid[i], NULL, &philosopher, &philo) != 0)
 			printf("thread broky\n");
 		i++;
 	}
