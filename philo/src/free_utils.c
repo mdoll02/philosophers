5d1e7e6827bb464aa22b00343db5cd54135aafa9
mdoll <mdoll@student.42wolfsburg.de>
2023-04-20 11:56:30 +0200

    fix: added mutex destroy

--- philo/src/free_utils.c
+++ philo/src/free_utils.c
@@ -15,8 +15,11 @@
 
 void	free_data(t_data *data)
 {
+	while (data->number_of_philo--)
+		pthread_mutex_destroy(&data->forks[data->number_of_philo]);
 	free(data->forks);
 	free(data->tid);
+	pthread_mutex_destroy(&data->display);
 }
 
 void	free_philo(t_philo **philo)
